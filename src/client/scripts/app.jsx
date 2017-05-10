a0c15af8940b583f21f344824c0fdee5af16d1c1
Florian Liss <flo.liss@tum.de>
2017-05-10 22:03:43 +0200

    Redesigned…

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -3,11 +3,15 @@ import styles from "../style.css";
 import {Bond} from 'oo7';
 import {RRaisedButton, Rspan, TextBond, HashBond} from 'oo7-react';
 import {formatBlockNumber, formatBalance, isNullData, makeContract} from 'oo7-parity';
-import {TransactionProgressBadge} from 'parity-reactive-ui';
+import {TransactionProgressBadge, AccountIcon} from 'parity-reactive-ui';
 import {List, ListItem} from 'material-ui/List';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import Subheader from 'material-ui/Subheader';
+import Chip from 'material-ui/Chip';
+//import {blue300, indigo900} from 'material-ui/styles/colors';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 
 import {sha3_256} from 'js-sha3';
 
@@ -107,7 +111,7 @@ export class App extends React.Component {
     this.owner = new Bond;
     this.id = new Bond;
     this.hash = new Bond;
-		this.valid = false;
+    this.valid = false;
     this.state = {
       tx: null
     };
@@ -118,48 +122,55 @@ export class App extends React.Component {
 
     return (
       <div>
-        <div
-          id="drop_zone"
-          onDragOver={this.handleDragOver.bind(this)}
-          onDrop={this.handleFileSelect.bind(this)}>
-          {this.file ?
-            <span>{this.file.name}, {this.file.type} - {this.file.size} Bytes</span> :
-            "Drop file here"
-          }
-        </div>
-        <RRaisedButton label="Log what's stored in this.file" onClick={() => console.log(this.file)} />
+        <Card>
+          <CardHeader title="Enter your Testimony Hash" subtitle={<TransactionProgressBadge value={this.state.tx}/>} actAsExpander={false} showExpandableButton={false}/>
+          <CardActions>
+            <div id="drop_zone" onDragOver={this.handleDragOver.bind(this)} onDrop={this.handleFileSelect.bind(this)}>
+              {this.file
+                ? <span>{this.file.name}, {this.file.type}
+                    - {this.file.size}
+                    Bytes</span>
+                : "Drop file here"
+}
+            </div>
+            <RaisedButton primary={true} label="Register as new testimonial" onClick={() => this.setState({
+              tx: this.contract.create(this.file.hash)
+            })} fullWidth={true}/>
+          </CardActions>
+        </Card>
 
-
-        <HashBond bond={this.hash} floatingLabelText='Hash'/>
-
-        <RRaisedButton label="Register as new testimonial" onClick={() => this.setState({
-          tx: this.contract.create(this.hash)
-        })}/>
-          <Rspan> {this.contract.isValid(this.hash).map(v => v.toString())} </Rspan>
+        <Rspan>
+          {this.contract.isValid(this.hash).map(v => v.toString())}
+        </Rspan>
         <div style={{
           marginTop: '1em'
         }}>
-          <TransactionProgressBadge value={this.state.tx}/>
         </div>
         <Rspan>
-        <div>
-          <List>
-          <Subheader>Recent Events</Subheader>
-          <Rspan>
-            {this.tests.map(v => v.map(function(ele) {
-              return (
+          <div>
+            <List>
+              <Subheader>Recent Events</Subheader>
+              <Rspan>
+                {this.tests.map(v => v.map(function(ele) {
+                  return (
 
-                    <ListItem rightIconButton={<RaisedButton label="Update" onClick={() => this.setState({
-                      tx: this.contract.update(ele.testimonyID, this.hash)
-                    })}/>} primaryText={'From ' + ele.from}
-                    secondaryText={'TID: ' + ele.testimonyID.toString() + ', Hash: ' + ele.hash}>
-                    </ListItem>
+                    <ListItem leftAvatar={
+                      <AccountIcon
+            	         style={{width: '1.2em', verticalAlign: 'bottom', marginLeft: '1ex'}}
+            	         key={ele.from}
+            	         address={ele.from}
+                         />
+                       } rightIconButton={< RaisedButton label = "Update" onClick = {
+                      () => this.setState({
+                        tx: this.contract.update(ele.testimonyID, this.hash)
+                      })
+                    } />} primaryText={'From ' + ele.from} secondaryText={'TID: ' + ele.testimonyID.toString() + ', Hash: ' + ele.hash}></ListItem>
 
-              )
-            }, this), this)}
-          </Rspan>
-          </List>
-        </div>
+                  )
+                }, this), this)}
+              </Rspan>
+            </List>
+          </div>
         </Rspan>
       </div>
     );
@@ -178,7 +189,7 @@ export class App extends React.Component {
       output = {
         name: f.name,
         type: f.type || "n/a",
-        size: f.size,
+        size: f.size
       };
       this.file = output;
 
@@ -193,8 +204,8 @@ export class App extends React.Component {
           // TODO: Substitute the following workaround with something react-like
           // document.getElementById('hash_field').value = "0x" + that.file.hash.toString();
           // that.hash = new Bond("0x" + that.file.hash);
-          document.getElementById('drop_zone').innerHTML = "<span style={color:#FFF;font-size:100%}>"+that.file.name+", "+that.file.type+" - "+that.file.size+" Bytes: "+that.file.hash+"</span>";
-         }
+          document.getElementById('drop_zone').innerHTML = "<span style={color:#FFF;font-size:100%}>" + that.file.name + ", " + that.file.type + " - " + that.file.size + " Bytes: " + that.file.hash + "</span>";
+        }
       };
 
       reader.readAsBinaryString(f);
@@ -205,8 +216,8 @@ export class App extends React.Component {
   }
 
   handleDragOver(evt) {
-      evt.stopPropagation();
-      evt.preventDefault();
-      evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
   }
 }
